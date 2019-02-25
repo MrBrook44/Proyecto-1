8592ae12c39a7c811d9e317c886dd78337872973
Ricardo <43780817+MrBrook44@users.noreply.github.com>
2019-02-25 19:07:01 +0100

    Proyecto Antiguo
    He creado este repositorio nuevo donde he subido todo lo que había hecho en el anterior con Nico para que quede más claro.

---
+++ Assets/Scripts/BumpOnTop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpOnTop : MonoBehaviour
+{
+
+    //cuando el jugador salte sobre este objeto, lo destruirá
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //el jugador tiene que tener un componente PlayerMovement (para que solo pueda ser el jugador)
+        if (other.gameObject.GetComponent<PlayerMovement>())
+        {
+            //buscamos la normal y calculamos el ángulo entre la normal y el vector Up
+            ContactPoint2D contact = other.contacts[0];
+            float angle = Vector2.Angle(contact.normal, transform.up);
+
+            //si el ángulo es de unos 180º (el jugador cae verticalmente), destruye al enemigo
+            if (Mathf.Approximately(angle, 180))
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed, jumpForce;
+    bool jump;
+    float speedX;
+    Rigidbody2D rb;
+
+    //Obtenemos el Rigidbody del jugador para modificar su velocidad
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    //En el Update() declaramos los "controles" del jugador, para desplazarse en el eje X y para saltar en el eje Y
+    void Update()
+    {
+        speedX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f) { jump = true; }
+    }
+
+    //Declaramos la velocidad del jugador en el eje X y en el eje Y
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed * speedX, rb.velocity.y);
+        if (jump)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jump = false;
+        }
+    }
+}
