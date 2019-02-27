ccd71a3869e841156085a466a71f4c7d41bf9930
Ricardo <43780817+MrBrook44@users.noreply.github.com>
2019-02-27 19:35:26 +0100

    Nuevo script funcional.
    Si te parece correcto hacemos una nueva brunch solo con este script y luego mergeamos esa y esta la borramos para quitar el script anterior y basurilla y dejamos solo el script bueno y la escena que funciona.

---
+++ Assets/Scripts/PingPongMoveBeta.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongMoveBeta : MonoBehaviour {
+
+    public Transform pointB;
+    public float speed;
+    private Vector2 initialPos;
+    private Vector2 finalPos;
+    private Vector2 dir;
+    int state;
+
+    void Start()
+    {
+        initialPos = transform.position;
+        finalPos = pointB.position;
+        state = GetState(initialPos,finalPos);
+        dir = new Vector2(finalPos.x - initialPos.x, finalPos.y - initialPos.y);
+    }
+    void Update()
+    {
+        if (ChangeSpeed(initialPos,finalPos,transform.position,state))
+        {
+            speed *= -1;
+        }
+        transform.Translate(speed * Time.deltaTime * dir);
+    }
+
+    static bool ChangeSpeed(Vector2 a, Vector2 b, Vector2 position, int state)
+    {
+        switch (state)
+        {
+            case (1):
+                {
+                    if ((position.x < b.x && position.y > b.y) || (position.x > a.x && position.y < a.y)) return true;
+                    return false;
+                }
+            case (2):
+                {
+                    if ((position.x < b.x && position.y < b.y) || (position.x > a.x && position.y > a.y)) return true;
+                    return false;
+                }
+            case (3):
+                {
+                    if ((position.x > b.x && position.y > b.y) || (position.x < a.x && position.y < a.y)) return true;
+                    return false;
+                }
+            case (4):
+                {
+                    if ((position.x > b.x && position.y < b.y) || (position.x < a.x && position.y > a.y)) return true;
+                    return false;
+                }
+            case (5):
+                {
+                    if ( position.y < b.y || position.y > a.y) return true;
+                    return false;
+                }
+            case (6):
+                {
+                    if (position.y > b.y || position.y < a.y) return true;
+                    return false;
+                }
+            case (7):
+                {
+                    if (position.x > b.x || position.x < a.x) return true;
+                    return false;
+                }
+            case (8):
+                {
+                    if (position.x < b.x || position.x > a.x) return true;
+                    return false;
+                }
+            case (9):
+                {
+                    return false;
+                }
+        }
+        return true;
+    }
+    static int GetState(Vector2 a, Vector2 b)
+    {
+        if (a.x > b.x && a.y < b.y) { return 1; }
+        if (a.x > b.x && a.y > b.y) { return 2; }
+        if (a.x < b.x && a.y < b.y) { return 3; }
+        if (a.x < b.x && a.y > b.y) { return 4; }
+        if (a.x == b.x && a.y > b.y) { return 5; }
+        if (a.x == b.x && a.y < b.y) { return 6; }
+        if (a.x < b.x && a.y == b.y) { return 7; }
+        if (a.x > b.x && a.y == b.y) { return 8; }
+        return 9;
+    }
+
+}
