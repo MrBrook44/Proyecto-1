c74f1ad702b28e4917e694c1f684cd36cbd89f69
anasUCM <47886581+anasUCM@users.noreply.github.com>
2019-03-01 11:45:36 +0100

    Add files via upload

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,6 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed, jumpForce;
     bool jump;
-    float speedX;
     Rigidbody2D rb;
 
     //Obtenemos el Rigidbody del jugador para modificar su velocidad
@@ -18,18 +17,20 @@ public class PlayerMovement : MonoBehaviour
     //En el Update() declaramos los "controles" del jugador, para desplazarse en el eje X y para saltar en el eje Y
     void Update()
     {
-        speedX = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.D)) rb.velocity = new Vector2(speed, rb.velocity.y);
+        else if (Input.GetKey(KeyCode.A)) rb.velocity = new Vector2(-speed, rb.velocity.y);
+
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f) { jump = true; }
     }
 
     //Declaramos la velocidad del jugador en el eje X y en el eje Y
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(speed * speedX, rb.velocity.y);
         if (jump)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             jump = false;
         }
+
     }
 }
