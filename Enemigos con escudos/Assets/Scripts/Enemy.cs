6db660accd63f4d35e810a00a4b30431d17c2394
jorgmo02 <47861220+jorgmo02@users.noreply.github.com>
2019-03-02 09:58:15 +0100

    Add files via upload
    Añadidos los enemigos y las instancias de sus escudos (es decir, el método que sirve para asignarles protecciones)

---
+++ Enemigos con escudos/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool leftShield, rightShield, topShield;
+    public GameObject shield;
+    public GameObject helmet;
+    private Transform trf;
+
+    void Start()
+    {
+        trf = GetComponent<Transform>();
+        EquipShield(leftShield, rightShield, topShield);
+    }
+
+    private void EquipShield(bool left, bool right, bool top)
+    {
+        if (top)
+        {
+            GameObject newHelmet;
+            newHelmet = Instantiate(helmet, new Vector2(transform.position.x, transform.position.y + transform.localScale.y), Quaternion.identity, trf);
+            newHelmet.transform.Rotate(new Vector3(0, 0, 0));
+        }
+        if (left)
+        {
+            GameObject newShield;
+            newShield = Instantiate(shield, new Vector2(transform.position.x - transform.localScale.x, transform.position.y), Quaternion.identity, trf);
+            newShield.transform.Rotate(new Vector3(0, 0, -90));
+        }
+        if (right)
+        {
+            GameObject newShield;
+            newShield = Instantiate(shield, new Vector2(transform.position.x + transform.localScale.x, transform.position.y), Quaternion.identity, trf);
+            newShield.transform.Rotate(new Vector3(0, 0, 90));
+        }
+    }
+}
